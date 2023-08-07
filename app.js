fe3b318f75ff60dee21fbae98983aa6461f38f31
hilal dedek <hdedek03@gmail.com>
2023-08-07 17:37:32 +0300

    third commit

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const shippingCost=20;
 const shippingCostFree=150;
+const cart=document.querySelector(".ul");
+const cartList=[];
+// console.log(document.querySelector(".plus").parentElement.parentElement.previousElementSibling.previousElementSibling.innerText);
 
 window.addEventListener("load",()=>{
     localStorage.setItem("shippingCost",shippingCost);
@@ -13,9 +16,31 @@ const productsDiv= document.querySelector(".products");
 productsDiv.addEventListener("click",(event)=>{
     if(event.target.classList.contains("minus")){
         console.log("minus btn clicked");
+        if(event.target.parentElement.nextElementSibling.children[0].innerText!=1){
+            event.target.parentElement.nextElementSibling.children[0].innerText--;
+        }
+        else{
+            if(confirm("product will be removed?")){
+                
+            }
+        }
     }
     else if(event.target.classList.contains("plus")){
         console.log("plus button is clicked");
+        event.target.parentElement.previousElementSibling.children[0].innerText++;
+        const name=event.target.parentElement.parentElement.previousElementSibling.previousElementSibling.innerText;
+        if(cartList.length==0){
+            cartList.push(name);
+        }
+        else{
+            while(cartList.indexOf(name)==-1){
+                cartList.push(name);
+            }
+        }
+        const cartP=document.createElement("p");
+        const cartLi=document.createElement("li");
+        console.log(cartList);
+        
     }
     else if(event.target.classList.contains("remove")){
         console.log("remove button is clicked");
