f9e981e8f51bade3f148285c9fd0424b7afef738
hilal dedek <hdedek03@gmail.com>
2023-08-13 16:31:01 +0300

    problems fixed

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const nameList=[];
 var nameListCounter=0;
 var counter=-1;
 var total=0;
-var buffer=0;
 var ctr=0;
+var urunctr=0;
 
 window.addEventListener("load",()=>{
     localStorage.setItem("shippingCost",shippingCost);
@@ -17,6 +17,12 @@ window.addEventListener("load",()=>{
     sessionStorage.setItem("shippingCost",shippingCost);
     sessionStorage.setItem("shippingCostFree",shippingCostFree);
 });
+const alertP=document.getElementById("alert");
+const alertMessage=document.createElement("p");
+if(nameList.length==0){
+            alertMessage.innerText="SEPETİNİZDE ÜRÜN YOK"
+            alertP.appendChild(alertMessage);
+        }
 const productsDiv= document.querySelector(".products");
 productsDiv.addEventListener("click",(event)=>{
     ctr=0;
@@ -25,15 +31,21 @@ productsDiv.addEventListener("click",(event)=>{
         const name2=event.target.parentElement.parentElement.parentElement.children[0].innerText;
         const total2=event.target.parentElement.parentElement.parentElement.children[1].children[0].innerText;
         if(event.target.parentElement.nextElementSibling.children[0].innerText==0){
-           console.log("sepetinizde böyle bir ürün yoktur.");
+           alert("sepetinizde böyle bir ürün yoktur.");
         }
         else if(event.target.parentElement.nextElementSibling.children[0].innerText==1){
-            if(confirm("product will be removed?")){
+            if(confirm(`${name2} will be removed?`)){
                     event.target.parentElement.nextElementSibling.children[0].innerText--;
                     var x=cartList.indexOf(`${name2}`);
                     delete cartList[x];
                     const deleteUrun=document.getElementById(`delete${name2}`);
                     deleteUrun.remove();
+                    total -= total2;
+                    console.log(total);
+                    for(let i=0;i<nameList.length;i++){
+                        var y=nameList.indexOf(`${name2}`);
+                        delete nameList[y];
+                    }
                     }
 
         }
@@ -41,7 +53,7 @@ productsDiv.addEventListener("click",(event)=>{
             event.target.parentElement.nextElementSibling.children[0].innerText--;
             console.log(name2);
             var x=cartList.indexOf(`${name2}`);
-            console.log(x);
+            // console.log(x);
             delete cartList[x];
             const urun1=document.getElementById(`${name2}`);
             for(let i=0;i<cartList.length;i++){
@@ -50,7 +62,6 @@ productsDiv.addEventListener("click",(event)=>{
                 }
             }
             urun1.innerText=ctr;
-
             console.log(cartList);
             total -= total2;
             console.log(total);
@@ -58,19 +69,18 @@ productsDiv.addEventListener("click",(event)=>{
     }
     else if(event.target.classList.contains("plus")){
         console.log("plus button is clicked");
+        alertMessage.remove();
         event.target.parentElement.previousElementSibling.children[0].innerText++;
         const name=event.target.parentElement.parentElement.previousElementSibling.previousElementSibling.innerText;
         const total1=event.target.parentElement.parentElement.parentElement.children[1].children[0].innerText
         total+=Number(total1);
-        // console.log(total);
+        console.log(total);
         cartList.push(name);
-        
-        buffer=nameList.length;
         const a= event.target.parentElement.previousElementSibling.children[0].innerText;
-        // console.log(document.querySelectorAll(".plus").);
         ctr=0;
         if(nameList.length==0){
             nameList.push(name);
+            console.log("namelist",nameList);
             const cartLi1=document.createElement("li");
             const cartH1=document.createElement("h3");
             const cartH2=document.createElement("h5");
@@ -84,6 +94,7 @@ productsDiv.addEventListener("click",(event)=>{
         }
         else if(nameList.indexOf(name)==-1){
             nameList.push(name);
+            console.log("namelist",nameList);
             const cartLi2=document.createElement("li");
             const cartH3=document.createElement("h3");
             const cartH4=document.createElement("h5");
@@ -101,23 +112,53 @@ productsDiv.addEventListener("click",(event)=>{
                     ctr++;
                 }
             }
-            console.log(ctr);
+            // console.log(ctr);
             const urun=document.getElementById(`${name}`);
             urun.innerText=ctr;
             
         }     
-        console.log(cartList);
+        console.log("CARTlist",cartList);
         
     }
     else if(event.target.classList.contains("remove")){
-        console.log("remove button is clicked");
         var b=event.target.parentElement.previousElementSibling.children[1].children[0];
         const name3=event.target.parentElement.parentElement.children[0].innerText;
-        console.log(name3);
-        console.log("b",b);
-        b.innerText=0;
-        const deleteUrun2=document.getElementById(`delete${name3}`);
-        deleteUrun2.remove();
+        const total3=event.target.parentElement.parentElement.children[1].children[0].innerText;
+        if(confirm(`${name3} will be removed?`)){
+            console.log("remove button is clicked");
+            for(let j=0;j<cartList.length;j++){
+                if(cartList[j]==`${name3}`){
+                        total-=total3;
+                        console.log(total);
+                    }
+            }
+            for(let i=0;i<cartList.length;i++){
+                var y=cartList.indexOf(`${name3}`);
+                delete cartList[y];
+            }
+            
+            for(let i=0;i<nameList.length;i++){
+                var y=nameList.indexOf(`${name3}`);
+                delete nameList[y];
+            }
+            console.log(total);
+            console.log("CARTlist",cartList);
+            console.log("namelist",nameList);
+            b.innerText=0;
+            const deleteUrun2=document.getElementById(`delete${name3}`);
+            deleteUrun2.remove();
+        }
+        // for(let i=0;i<nameList.length;i++){
+        //     if(typeof cartList[i]===undefined){
+        //         urunctr++;
+        //     }
+        // }
+        // console.log(urunctr);
+        // if(urunctr==nameList.length){
+        //     const alertMessage2=document.createElement("p");
+        //     alertMessage2.innerText="SEPETİNİZDE ÜRÜN YOK"
+        //     alertP.appendChild(alertMessage2);
+        // }
     }
     else{
         console.log("other element is clicked");
